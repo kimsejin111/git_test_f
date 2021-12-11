2e5a8729ed1718d5bd475ac423aa6fbdd2f2348b
kimsejin111 <kimtpwjd@ajou.ac.kr>
2021-12-11 22:38:07 +0900

    first commit

---
+++ File_name_change.py
@@ -0,0 +1,16 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+file_path = 'C:/Users/lenovo/Desktop/ML_PRATICE/speed_limit/valid/100'
+file_names = os.listdir(file_path)
+file_names
+
+i = 1
+for name in file_names:
+    src = os.path.join(file_path, name)
+    dst = str(i)+'(v_3)' + '.png'
+    dst = os.path.join(file_path, dst)
+    os.rename(src, dst)
+    i += 1
+
